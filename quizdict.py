f5486968234b2c047b46cfea6cf7904efdf8d6b4
Linus <linus.zand@gmail.com>
2022-10-14 01:11:50 +0200

    Kod klar med funktioner

--- quizdict.py
+++ quizdict.py
@@ -2,51 +2,81 @@
 import json
 import requests
 import pprint
-# Ta ut information från url
-# lägga varje fråga och frågans alla svarsalternativ i en egen variabel
-# Göra så att variabeln har ett korrekt svar.
 
 
-url = 'https://bjornkjellgren.se/quiz/v1/questions'
-results = requests.get(url)
-data = results.json()
+def get_questions_data():
+    url = 'https://bjornkjellgren.se/quiz/v1/questions'
+    results = requests.get(url)
+    data = results.json()
+    pprint.pprint(data)
+    return data
 
-a = 0
-score = 0
-for question in data['questions']:
-    a = a + 1
-    print(f"Fråga {a}.")
-    print(question['prompt'])
 
+def print_answers_loop(question):
     for i, answer in enumerate(question['answers'], start=1):
         print(f"{i}. {answer['answer']}")
-        # i = i + 1
-    user_input = int(input(">>>"))
 
+
+def get_answers_data(question):
     answers = question['answers']
+    return answers
+
+
+def get_selected_answer(answers, user_input):
     selected_answer = answers[user_input - 1]
-
-    if selected_answer['correct']:
-        score = score + 1
-        print("")
-        print(f"Rätt svar! Du har {score} poäng.\n")
-        # print(selected_answer['answer'])
-    else:
-        for i, answer in enumerate(question['answers']):
-            # print(answers[i]['answer'])
-            # print(answers[i]['correct'])
-            correct_answer = answers[i]['answer']
-            if answers[i]['correct']:
-                print("")
-                print(f"Fel! Rätt svar är: {correct_answer}")
-                print(f"Du har {score} poäng.\n")
+    return selected_answer
 
 
-print(f"Du har sammanlagt fått {score} poäng av {a}")
+def score_plus_and_print_correct_answer(score):
+    score = score + 1
+    print("")
+    print(f"Rätt svar! Du har {score} poäng.\n")
+    return score
+
+
+def get_correct_answer(answers, i):
+    correct_answer = answers[i]['answer']
+    return correct_answer
+
+
+def score_plus_and_print_wrong_answer(correct_answer, score):
+    print("")
+    print(f"Fel! Rätt svar är: {correct_answer}")
+    print(f"Du har {score} poäng.\n")
 
 
 def main():
-    pass
+    data = get_questions_data()  # Tar ut informationen från url:en med hjälp av requests och gör om det till json() så att vi kan använda informationen.
+
+    score = 0  # En variabel med värdet 0 int
+
+    for a, question in enumerate(data['questions'], start=1): # En loop som tar datan från en lång lista med frågor
+        print(f"Fråga {a}.") # printar ut Fråga. och index som har start 1
+        print(question['prompt'])  # printar ut alla frågar var för sig i en loop
+
+        print_answers_loop(question)  # Skriver ut svaren för varje enskild fråga
+
+        user_input = int(input(">>>"))  # Variabeln user_input får det värde vi väljer att skriva in
+
+        answers = get_answers_data(question)  # Tar ut all data i 'answer' nycklarna
+
+        selected_answer = get_selected_answer(answers, user_input)  # Det vi skrev in i user_input variabeln konsollen. Det svaret du skrev in: (1,2,3 eller 4) minus ett i index
+
+        if selected_answer['correct']:  # Om det svaret man gissade på har värdet True i 'correct' nycklarna så kör den koden nedan
+
+            score = score_plus_and_print_correct_answer(score)  # Printar ut om man hade rätt samt poäng efter svaret
+
+        else:
+            for i, answer in enumerate(question['answers']):  # en loop som tar ut datan från nyckeln 'answers'
+
+                correct_answer = get_correct_answer(answers, i)  # Tar ut alla nyckar från 'answer' som vi sedan använder i if satsen nedan
+
+                if answers[i]['correct']:  # Loopar igenom varje 'correct' nyckel och printar bara ut det om värdet är True
+
+                    score_plus_and_print_wrong_answer(correct_answer, score)  # Printar ut om man hade fel och vilket svar som var rätt samt vilket poäng man har efter svaret
+
+    print(f"Du har sammanlagt fått {score} poäng av {a}. ")
 
 
-main()
+if __name__ == '__main__':
+    main()
