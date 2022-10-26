87834c60d035771e75956698ef78c882428f1b57
Linus <linus.zand@gmail.com>
2022-10-26 14:06:37 +0200

    Case 5

--- quizdict.py
+++ quizdict.py
@@ -1,11 +1,11 @@
 # coding=utf8
-from getmethods import get_questions_data, print_answers_loop, get_answers_data, get_selected_answer, \
-    score_plus_and_print_correct_answer, get_correct_answer, score_plus_and_print_wrong_answer
+# from getmethods import get_questions_data, print_answers_loop, get_answers_data, get_selected_answer, \
+#     score_plus_and_print_correct_answer, get_correct_answer, score_plus_and_print_wrong_answer
 import json
 import requests
 import pprint
 import random
-random.sample
+
 
 # 1. Programmet kraschar ibland om jag råkar trycka på retur utan att ha skrivit in något eller om jag skriver något annat än siffror. Om frågan har 4 svar skall bara siffrorna 1-4 accepteras som input.
 # 2. Välj ut 10 slumpmässiga frågor av dem ni får från APIet.
@@ -20,26 +20,26 @@ def main():
     url = 'https://bjornkjellgren.se/quiz/v2/questions'
     results = requests.get(url)
     data = results.json()
+    pprint.pprint(data)
 
     wrong_questions_list = []
 
     score = 0
     for a, question in enumerate(random.sample(data['questions'], 10)):  # En loop med ett index(a) som startar på 1, och som tar ut all data från nyckeln 'questions'
 
-        q = data['questions'][a] # q tar ut varje element från nyckeln 'answers' först data['questions'][0], sen data['questions'][1] osv...
+        # q = data['questions'][a] # q tar ut varje element från nyckeln 'answers' först data['questions'][0], sen data['questions'][1] osv...
+        # q gjorde så att det fel uppstod, men jag vet inte exakt varför. När jag tog bort q fixade allt sig i alla fall!
 
+        hela = int(question['times_asked']) # Tar nyckeln 'times_asked' som har ett nummer i sig och konverterar det till int
+        delen = int(question['times_correct']) # Tar nyckeln 'times_correct' som har ett nummer i sig och konverterar det till int
 
-        hela = int(question['times_asked'])
-        delen = int(question['times_correct'])
+        percent = int(get_percent(delen, hela))  #  Kallar på get_percent funktionen som har som uppgift att dividera delen i det hela och lägger resultat i en variabel som heter percent
 
-        percent = int(get_percent(delen, hela))
+        quest = (question['prompt']) # Tar nyckeln 'prompt' och lägger det i variabeln quest
 
-        quest = (q['prompt'])
+        print(f'Fråga. {a+1} [{percent}% har svarat rätt] {quest} ') # printar ut alla
 
-
-        print(f'Fråga. {a + 1} [{percent}% har svarat rätt] {quest} ')
-
-        answers = q['answers']
+        answers = question['answers']
         for i, answer in enumerate(answers):  # En loop med ett index(i) som startar på 1, och som tar ut element från nyckeln 'answers'
             print(f"{i + 1}. {answer['answer']}")
 
@@ -50,9 +50,9 @@ def main():
 
             except ValueError:
                 pass
-            if user_input in range(1, len(q['answers']) + 1):
+            if user_input in range(1, len(question['answers']) + 1):
                 break
-            print(f"Skriv ett nummer mellan 1 - {len(q['answers'])}")
+            print(f"Skriv ett nummer mellan 1 - {len(question['answers'])}")
 
         answers = question['answers']
 
@@ -72,7 +72,7 @@ def main():
                     print("")
                     print(f"Fel! Rätt svar är: {correct_answer}")
                     print(f"Du har {score} poäng.")
-                    wrong_questions_list.append((q['prompt'], your_answer, correct_answer)) # tar ut alla frågor, alla rätta svar och varje svar användaren gav på varje fråga och lägger sedan in dem i listan
+                    wrong_questions_list.append((question['prompt'], your_answer, correct_answer)) # tar ut alla frågor, alla rätta svar och varje svar användaren gav på varje fråga och lägger sedan in dem i listan
             print(f"Ditt svar: {your_answer}\n")
 
     print(f"Du har sammanlagt fått {score} poäng av {a + 1}\n")
