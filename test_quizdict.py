18f94f66b6d31eb3763886a32b9b5e0d196012f8
Linus <linus.zand@gmail.com>
2022-11-07 00:07:18 +0100

    Case 6

---
+++ test_quizdict.py
@@ -0,0 +1,13 @@
+import pytest
+from quizdict import get_percent
+
+
+def test_get_percent_low():
+    assert get_percent(1, 200) == 0.5
+
+def test_get_percent_high():
+    assert get_percent(1000000000, 2000000000) == 50
+
+def test_get_percent_b_a():
+    assert get_percent(2, 1) == 200
+
